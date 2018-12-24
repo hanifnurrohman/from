e15839d314316483d7d8f67a740b7160fc5d1cd9
hanifnurrohman <46113812+hanifnurrohman@users.noreply.github.com>
2018-12-25 06:02:53 +0700

    Add files via upload

---
+++ From.php
@@ -0,0 +1,38 @@
+<html>
+     <head>
+	 <meta charset="utf-8">
+     <title>HanifWabDeveloper</title>
+     <link rel="stylesheet" type="text/css" href="fromsy.css">
+    </head>
+      
+<body>
+<div class="box">
+
+  <h2>LOGIN</h2>
+
+  <form>
+
+      <div class="inputBox">
+      	<input type="text"  name=""  required="">
+      	<label>UserName</label>
+      </div>
+
+      <div class="inputBox">
+      	<input type="password"  name=""  required="">
+      	<label>Password</label>
+      </div>
+
+         <input type="submit" name=""   value="submit"> 
+
+     
+  </form>
+</div>
+<img src="">
+
+</body>
+
+
+
+
+
+</html>
